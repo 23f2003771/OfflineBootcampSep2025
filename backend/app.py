ddf6298392906808a82449c201df3e2c960b5677
Shikhar Singh <neuralnetwork@duck.com>
2025-10-28 17:43:54 +0530

    Initial Commit

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+## This is what we were doing in mad1
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# This is what we NEED to do in mad2 (i.e, returning data instead of HTML)
+class HelloWorld(Resource):
+    def get(self):
+        return {'message': 'Hello, World!'}
+    
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
